7492cd02f9cfca0c37bfe648db6aa09d6598aa39
nsywptr <119945652+nsywptr@users.noreply.github.com>
2023-04-15 14:35:11 +0700

    Add files via upload

---
+++ nasywa.js
@@ -0,0 +1,43 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+
+            });
+        };
+    });
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+ScrollReveal({ 
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.Home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.Home-img, .About, .myExperience-container, .contactMe form', { origin: 'bottom' });
+ScrollReveal().reveal('.Home-content h1, .About-img', { origin: 'left' });
+ScrollReveal().reveal('.Home-content p, .About-content', { origin: 'right' });
